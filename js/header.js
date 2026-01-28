0ab706bd03a720db15edf8d772201ac80af80f5e
fujikawa <r.fujikawa.sys25@morijyobi.ac.jp>
2026-01-28 09:53:09 +0900

    scriptの修正

--- js/header.js
+++ js/header.js
@@ -1,14 +1,10 @@
-  const buttons = document.querySelectorAll('.tab-btn');
-  const contents = document.querySelectorAll('.tab-content');
 
-  buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      // ボタンの切り替え
-      buttons.forEach(b => b.classList.remove('active'));
-      btn.classList.add('active');
+  function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+}
 
-      // コンテンツの切り替え
-      contents.forEach(c => c.classList.remove('active'));
-      document.getElementById(btn.dataset.tab).classList.add('active');
-    });
-  });
+  function closeMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.remove("active");
+}
--- js/timetable.js
+++ js/timetable.js
@@ -7,3 +7,18 @@ function tab_change() {
         $(".content").removeClass("show").eq(index).addClass("show");
     });
 };
+
+  const buttons = document.querySelectorAll('.tab-btn');
+  const contents = document.querySelectorAll('.tab-content');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      // ボタンの切り替え
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+
+      // コンテンツの切り替え
+      contents.forEach(c => c.classList.remove('active'));
+      document.getElementById(btn.dataset.tab).classList.add('active');
+    });
+  });
